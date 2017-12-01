dc3f5e4c6d3766ab7e3b129004c1371c62cae2c0
Pierpaolo <plucarelli.94@gmail.com>
2017-12-01 17:25:42 +0000

    1d array

--- Network.java
+++ Network.java
@@ -4,41 +4,40 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Network {
-	private int[][] synapses;
+	private int[] synapses;
 	private int size;
 	private ArrayList<int[]> inputs;
 	private ArrayList<int[]> outputs;
 	public Network(int size) {
 		// create 2D array with zeros
-		this.size = size;
-		this.synapses = new int[size][size];
+		this.size = size*size;
+		this.synapses = new int[this.size];
 		this.inputs = new ArrayList<>();
 		this.outputs = new ArrayList<>();
 	}
 	
 	public void train(int[] inputs, int[] outputs) {
-		this.inputs.add(inputs);
-		this.outputs.add(outputs);
+		savePair(inputs, outputs);
 		int l = inputs.length;
 		for(int i = 0 ; i < l ; i++)
 			for(int j = 0 ; j < l ; j++)
-				this.synapses[i][j] = (inputs[i] & outputs[j]) | this.synapses[i][j];
+				synapses[i*l+j] = (inputs[i] & outputs[j]) | synapses[i*l+j];
 	}
 	
-	public int[] test(int[] inputs, boolean debug) {
-		if(debug)
-		System.out.println("Testing input: " + Arrays.toString(inputs));
-		int[] results = new int[size];
-		int[] originalPattern = this.inputs.get(getClosestInputIndex(inputs));
-		int u = this.getU(inputs, originalPattern);
-		for(int i = 0 ; i < size ; i++) {
-			int synapSum = 0;
-			for(int j = 0 ; j < size ; j++)
-				synapSum += inputs[j] * synapses[j][i];
-			results[i] = (synapSum >= u ? 1 : 0);
-		}
-		return results;
-	}
+	// public int[] test(int[] inputs, boolean debug) {
+	// 	if(debug)
+	// 	System.out.println("Testing input: " + Arrays.toString(inputs));
+	// 	int[] results = new int[size];
+	// 	int[] originalPattern = this.inputs.get(getClosestInputIndex(inputs));
+	// 	int u = this.getU(inputs, originalPattern);
+	// 	for(int i = 0 ; i < size ; i++) {
+	// 		int synapSum = 0;
+	// 		for(int j = 0 ; j < size ; j++)
+	// 			synapSum += inputs[j] * synapses[j][i];
+	// 		results[i] = (synapSum >= u ? 1 : 0);
+	// 	}
+	// 	return results;
+	// }
 	
 	private int hammingDistance(int[] i, int[] i2) {
 		int dist = 0;
@@ -62,7 +61,7 @@ public class Network {
 	}
 	
 	public String printSize() {
-		return (int)Math.pow(this.size,2) + " synapse network (" + this.size + "x" + this.size + ")";
+		return (int)Math.sqrt(this.size) + " input network (" + this.size + "x" + this.size + ")";
 	}
 	
 	private int getU(int[] inputs, int[] original) {
@@ -78,15 +77,20 @@ public class Network {
 	public double getLoadParameter() {
 		return this.inputs.size() / (double)this.size;
 	}
-	
-	public double synapsesLoad() {
-		int activeSynapseCount = 0;
-		for(int i = 0 ; i < this.size; i++) {
-			for(int j = 0 ; j < this.size ; j++) 
-				activeSynapseCount += this.synapses[i][j];
-		}
-		return activeSynapseCount / Math.pow(this.size, 2);
+
+	private void savePair(int[] inputs, int[] outputs){
+		this.inputs.add(inputs);
+		this.outputs.add(outputs);
 	}
+	
+	// public double synapsesLoad() {
+	// 	int activeSynapseCount = 0;
+	// 	for(int i = 0 ; i < this.size; i++) {
+	// 		for(int j = 0 ; j < this.size ; j++) 
+	// 			activeSynapseCount += this.synapses[i][j];
+	// 	}
+	// 	return activeSynapseCount / Math.pow(this.size, 2);
+	// }
 	
 	public void pop() {
 		this.inputs.remove(inputs.size() -1);
@@ -95,9 +99,6 @@ public class Network {
 	
 	@Override
 	public String toString() {
-		String output = "";
-		for(int i = 0 ; i < size ; i ++)
-			output += Arrays.toString(this.synapses[i]) + "\n";
-		return output;
+		return Arrays.toString(this.synapses) + "\n";
 	}
 }
